01f3d0427988b5adb09d5572810c93bb5c265677
uzbai <uzbai87@gmail.com>
2025-05-05 16:20:08 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const nasiyalar = [
+  {
+    ism: "Farxod",
+    tarix: [
+      { sana: "01.05.2025 12:00", tur: "olindi", summa: 5000000 },
+      { sana: "03.05.2025 10:00", tur: "berdi", summa: 4000000 }
+    ]
+  },
+  {
+    ism: "Alisher",
+    tarix: [
+      { sana: "01.05.2025 08:00", tur: "olindi", summa: 3200000 },
+      { sana: "02.05.2025 18:00", tur: "berdi", summa: 5000000 }
+    ]
+  },
+  {
+    ism: "Sultonxo‘ja",
+    tarix: [
+      { sana: "03.05.2025 09:30", tur: "berdi", summa: 5000000 },
+      { sana: "03.05.2025 11:00", tur: "olindi", summa: 3232000 }
+    ]
+  }
+];
