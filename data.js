ae4c6999e316a775dc5eb580be9e0fdc1e017f76
uzbai <uzbai87@gmail.com>
2025-05-07 10:47:02 +0500

    Update data.js

--- data.js
+++ data.js
@@ -2,14 +2,14 @@ const nasiyalar = [
   {
     ism: "Farxod",
     tarix: [
-      { sana: "01.05.2025 12:00", tur: "olindi", summa: 5000000 },
+      { sana: "01.05.2025 12:00", tur: "oldi", summa: 5000000 },
       { sana: "03.05.2025 10:00", tur: "berdi", summa: 4000000 }
     ]
   },
   {
     ism: "Alisher",
     tarix: [
-      { sana: "01.05.2025 08:00", tur: "olindi", summa: 3200000 },
+      { sana: "01.05.2025 08:00", tur: "oldi", summa: 3200000 },
       { sana: "02.05.2025 18:00", tur: "berdi", summa: 5000000 }
     ]
   },
@@ -17,7 +17,7 @@ const nasiyalar = [
     ism: "Sultonxo‘ja",
     tarix: [
       { sana: "03.05.2025 09:30", tur: "berdi", summa: 5000000 },
-      { sana: "03.05.2025 11:00", tur: "olindi", summa: 3232000 }
+      { sana: "03.05.2025 11:00", tur: "oldi", summa: 3232000 }
     ]
   }
 ];
